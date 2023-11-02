1f58f1ea8e585bc17b5b0cbbdb248a3bfd257324
awaze7 <91365506+awaze7@users.noreply.github.com>
2023-11-02 23:02:26 +0530

    Update bb.py

--- bb.py
+++ bb.py
@@ -7,6 +7,9 @@ class Item:
 def knapsack_branch_and_bound(values, weights, capacity):
     items = []
 
+    max_value = 0
+    result_items = []
+    
     for i in range(len(values)):
         value = values[i]
         weight = weights[i]
@@ -18,7 +21,6 @@ def knapsack_branch_and_bound(values, weights, capacity):
     def get_bound(current, remaining_capacity, i):
         # Calculate the bound for the current node
         bound = current
-        total_weight = 0
         while i< len(items):
             if items[i].weight<=remaining_capacity:
                 bound += items[i].value
@@ -47,32 +49,31 @@ def knapsack_branch_and_bound(values, weights, capacity):
             selected_items.pop()
             branch_and_bound(i + 1, current_value, current_weight, selected_items)
 
-    max_value = 0
-    result_items = []
+
     branch_and_bound(0, 0, 0, [])
 
     return max_value, result_items
 
 # Example usage
 if __name__ == "__main__":
-#     values = [10, 40, 30, 50]
-#     weights = [5, 4, 6, 3]
-#     capacity = 10
+    values = [40,50,100,95,30] #[10, 40, 30, 50]
+    weights = [2,3.14,1.98,5,3]#[5, 4, 6, 3]
+    capacity = 10
+
     
-    capacity=int(input("Enter the weight capacity of Knapsack:"))
-    n=int(input("Enter the number of items:"))
-    values=[]
-    weights=[]
+#     capacity=int(input("Enter the weight capacity of Knapsack:"))
+#     n=int(input("Enter the number of items:"))
+#     values=[]
+#     weights=[]
     
-    for i in range(n):
-        values.append(int(input(f"Enter the value of item {i+1}:")))
-        weights.append(int(input(f"Enter the weight of item {i+1}:")))
+#     for i in range(n):
+#         values.append(int(input(f"Enter the value of item {i+1}:")))
+#         weights.append(int(input(f"Enter the weight of item {i+1}:")))
+
         
     # Solve the 0-1 Knapsack Problem using Branch and Bound
     max_value, selected_items = knapsack_branch_and_bound(values, weights, capacity)
 
-    
-    selected_items
     print("Selected items:")
     for i in selected_items:
         print(f"Item {i + 1} (Value: {values[i]}, Weight: {weights[i]})")

22ea19ec8ce93b0d30cfe85fba4835b95c1ad1b4
awaze7 <91365506+awaze7@users.noreply.github.com>
2023-11-03 12:29:31 +0530

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -4,19 +4,35 @@ def isSafe(board, row, col):
     for i in range(col):
         if board[row][i] == 1:
             return False
- 
+            
+    # Check this row on right side
+    for i in range(col,N):
+        if board[row][i]==1:
+            return False
+    
     # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1),
                     range(col, -1, -1)):
         if board[i][j] == 1:
             return False
- 
+    # Check lower diagonal on right side
+    for i, j in zip(range(row, N),
+                    range(col, N)):
+        if board[i][j] == 1:
+            return False
+    
     # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1),
+    for i, j in zip(range(row, N, 1), 
                     range(col, -1, -1)):
         if board[i][j] == 1:
             return False
- 
+            
+    # Check Upper diagonal on right side
+    for i, j in zip(range(row, -1, -1), 
+                    range(col, N)):
+        if board[i][j] == 1:
+            return False
+    
     return True
  
  
@@ -24,15 +40,18 @@ def solveNQUtilapproach1(board, col):
  
     if col >= N:
         return True
- 
+    
+    if col==first_queen_col:
+        return solveNQUtilapproach1(board, col + 1)
+    
     for i in range(N):
- 
-        if isSafe(board, i, col):
+            
+        if i!=first_queen_row and isSafe(board, i, col):
             board[i][col] = 1
  
             if solveNQUtilapproach1(board, col + 1) == True:
                 return True
-            
+
             board[i][col] = 0
  
     return False
@@ -42,21 +61,23 @@ def solveNQUtilapproach1(board, col):
 def printSolution(board):
     for i in range(N):
         for j in range(N):
-#             print(board[i][j], end=" ")
             if board[i][j]==1:
                 print("Q",end=" ")
             else:
                 print(".",end=" ")
         print()
-
+    print("----------------------------------------------------------------")
 
 def solveNQUtilapproach2(board, col):
     if (col >= N):
         return True
-
+    
+    if col==first_queen_col:
+        return solveNQUtilapproach2(board, col + 1)
+        
     for i in range(N):
-
-        if ((ld[i - col + N - 1] != 1 and
+        
+        if (i!=first_queen_row and (ld[i - col + N - 1] != 1 and
             rd[i + col] != 1) and cl[i] != 1):
 
             board[i][col] = 1
@@ -64,45 +85,38 @@ def solveNQUtilapproach2(board, col):
 
             if (solveNQUtilapproach2(board, col + 1)):
                 return True
-
+    
             board[i][col] = 0 
             ld[i - col + N - 1] = rd[i + col] = cl[i] = 0
 
     return False
-
-
-
+    
 def solveNQ(N):
     board1=[[0 for x in range(N)] for x in range(N)]
     
-     # Place the first queen at (0, 0)
-    first_queen_col = 0
-    board1[0][first_queen_col] = 1 #and then pass solveNQUtilapproach1(board1, first_queen_col+1)
+         # Place the first queen at (0, 0)
+    board1[first_queen_row][first_queen_col] = 1 #and then pass solveNQUtilapproach1(board1, first_queen_col+1)
+    board2=board1.copy()
     
-    ld[0-first_queen_col+N-1]=rd[0+first_queen_col]=cl[0]=1
-    board2=board1
-
-    if (solveNQUtilapproach1(board1, first_queen_col+1) == False): # TC:O(N!*N) SC:O(N)
-        print("Solution does not exist")
-        return False
-    print("Using approach 1: ")
+    solveNQUtilapproach1(board1, 0)
+    print("Approach 1:")
     printSolution(board1)
-
-    if (solveNQUtilapproach2(board2, first_queen_col+1) == False): # TC:O(N!) SC:O(N)
-        print("Solution does not exist")
-        return False
-
-    print("\nUsing approach 2: ")
-    printSolution(board2)
-
-    return True
-
-
-if __name__ == '__main__':
-    N=int(input("Enter n: "))
-    ld = [0] * N * 2
-
-    rd = [0] * N * 2
     
-    cl = [0] * N * 2 #to track rows
+    ld[first_queen_row-first_queen_col+N-1]=rd[first_queen_row+first_queen_col]=cl[first_queen_row]=1
+    solveNQUtilapproach2(board2,0)
+    print("Approach 2:")
+    printSolution(board2)
+   
+if __name__=="__main__":
+    # N=8
+    # first_queen_col = 4
+    # first_queen_row = 6
+    N = int(input("Enter N:"))
+    first_queen_col = int(input("Enter the col for the 1st queen to be placed:"))
+    first_queen_row = int(input("Enter the row for the 1st queen to be placed:"))
+    
+    ld=[0]*N*2
+    rd=[0]*N*2
+    cl=[0]*N*2
+    
     solveNQ(N)
